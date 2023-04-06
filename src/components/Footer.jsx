891fa5c493829be43d2294ea6f5a3151f0a47811
goodness5 <goodnesskolapo@gmail.com>
2023-04-07 00:20:00 +0100

    project initialized

---
+++ src/components/Footer.jsx
@@ -0,0 +1,22 @@
+
+ 
+
+import React from 'react';
+
+
+const Footer = ()=>{
+    return(
+        <div className="footer w-full absolute bottom-0 bg-blue-200">
+            <div className=" bg-black w-full">
+                <div className="row">
+                    <div className="col-md-12 text-white text-center">
+
+                        &copy;@devxspace 2023
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
