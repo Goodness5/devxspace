5b468ac037675c5a68d51a34e74a3fb3208e7594
goodness5 <goodnesskolapo@gmail.com>
2023-04-07 13:35:48 +0100

    changed from .tsx to .jsx

---
+++ src/pages/_app.jsx
@@ -0,0 +1,45 @@
+import '../../styles/globals.css';
+import '@rainbow-me/rainbowkit/styles.css';
+import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { AppProps } from 'next/app';
+import { configureChains, createClient, WagmiConfig } from 'wagmi';
+import { arbitrum, goerli, mainnet, optimism, polygon } from 'wagmi/chains';
+import { publicProvider } from 'wagmi/providers/public';
+import PageLayout from '../pagelayout/PageLayout';
+
+const { chains, provider, webSocketProvider } = configureChains(
+  [
+    mainnet,
+    polygon,
+    optimism,
+    arbitrum,
+    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true' ? [goerli] : []),
+  ],
+  [publicProvider()]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: 'RainbowKit App',
+  chains,
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+  webSocketProvider,
+});
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <PageLayout>
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
+        <Component {...pageProps} />
+      </RainbowKitProvider>
+    </WagmiConfig>
+    </PageLayout>
+  );
+}
+
+export default MyApp;
