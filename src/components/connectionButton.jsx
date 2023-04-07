adce7bf425941209d202a60cdc28c5488f1bec49
goodness5 <goodnesskolapo@gmail.com>
2023-04-07 13:43:13 +0100

    changed from .tsx to .jsx

---
+++ src/components/connectionButton.jsx
@@ -0,0 +1,8 @@
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import React from "react";
+
+const ConnectionButton = () => {
+  return <ConnectButton label="Connect wallet" />;
+};
+
+export default ConnectionButton;
