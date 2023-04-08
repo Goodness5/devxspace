8229927e8c798406a6d9999446fed9718e9113b7
vicsnet <adesanmiadebisi@gmail.com>
2023-04-08 11:35:30 +0100

    profile 50%

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Connect from "./Connect";
+import Link from 'next/link';
 
 const Navbar = () => {
   return (
     <div className='flex justify-center items-center py-3 border-b border-gray-300'>
         <button className="border border-green-400 rounded-lg py-2 px-4">Home</button>
+        <Link href="/profile" className="border border-green-400 rounded-lg py-2 px-4">Profile</Link >
         <button to={'/create-ballot'} className="mx-5 border border-green-400 rounded-lg py-2 px-4">Create Ballot</button>
     <div>
     <Connect />
