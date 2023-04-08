1ac6e7cdce248805939ca73a96446e09e4e9b738
goodness5 <goodnesskolapo@gmail.com>
2023-04-08 10:56:48 +0100

    progress

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react';
 import Connect from "./Connect";
 
 const Navbar = () => {
+  const [showDropdown, setShowDropdown] = useState(false);
+
+  const handleDropdownClick = () => {
+    setShowDropdown(!showDropdown);
+  };
+
   return (
-    <div className='flex justify-center items-center py-3 border-b border-gray-300'>
-        <button className="border border-green-400 rounded-lg py-2 px-4">Home</button>
-        <button to={'/create-ballot'} className="mx-5 border border-green-400 rounded-lg py-2 px-4">Create Ballot</button>
-    <div>
+    <div className='bg-blue-50 flex flex-row justify-end '>
+
+        <div className="flex items-center cursor-pointer p-4">
+          <span className="mr-2">Home</span>
+        </div>
+
+        <div className="flex items-center cursor-pointer p-4" onClick={handleDropdownClick}>
+          <span className="mr-2">Explore</span>
+        <div className={`flex flex-col justify-center bg-white shadow-lg ${showDropdown ? 'block' : 'hidden'}`}>
+          <a href="#" className="px-2 hover:bg-gray-100">Link 1</a>
+          <a href="#" className="px-2 hover:bg-gray-100">Link 2</a>
+          <a href="#" className="px-2 hover:bg-gray-100">Link 3</a>
+        </div>
+      </div>
+        <div className="flex items-center cursor-pointer p-4" onClick={handleDropdownClick}>
+          <span className="mr-2">Pages</span>
+         
+         <div className={`right-0 top-10 w-40 bg-white shadow-lg ${showDropdown ? 'block' : 'hidden'}`}>
+          <a href="#" className="px-2 hover:bg-gray-100">Link 1</a>
+          <a href="#" className="px-2 hover:bg-gray-100">Link 2</a>
+          <a href="#" className="px-2 hover:bg-gray-100">Link 2</a>
+          </ div>
+
     <Connect />
     </div>
     </div>
