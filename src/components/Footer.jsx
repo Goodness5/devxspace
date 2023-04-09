d0b02a5b519cde6bed354fd3f535e1289f206f51
goodness5 <goodnesskolapo@gmail.com>
2023-04-09 15:28:43 +0100

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 const Footer = ()=>{
     return(
-        <div className="footer w-full absolute bottom-0 bg-blue-200">
-            <div className=" bg-black w-full">
-                <div className="row">
-                    <div className="col-md-12 text-white text-center">
-
-                        &copy;@devxspace 2023
-                    </div>
-                </div>
+        <body class="relative min-h-screen">
+        <div class="footer w-full absolute bottom-0 bg-blue-200">
+          <div class="bg-black w-full">
+            <div class="row">
+              <div class="col-md-12 text-white text-center">
+                &copy;@devxspace 2023
+              </div>
             </div>
+          </div>
         </div>
+      </body>
+      
     )
 }
 
