7f188d9a518f193853486e44083ab37e736b0837
goodness5 <goodnesskolapo@gmail.com>
2023-04-13 11:25:25 +0100

    omo

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -5,6 +5,7 @@ import { arbitrum, goerli, mainnet, optimism, polygon, sepolia } from 'wagmi/cha
 import { publicProvider } from 'wagmi/providers/public';
 import '../../styles/globals.css';
 import Pagelayout from '../pagelayout/pagelayout';
+import { useAccount } from "wagmi";
 
 const { chains, provider, webSocketProvider } = configureChains(
   [
@@ -30,7 +31,33 @@ const wagmiClient = createClient({
   webSocketProvider,
 });
 
+
+
 function MyApp({ Component, pageProps }) {
+  const { data: account } = useAccount();
+  const address = account?.address;
+
+  useEffect(() => {
+    if (address) {
+      // Send the address to the login route
+      fetch('/api/login', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ address }),
+      })
+        .then((response) => {
+          if (response.ok) {
+            console.log('Successfully logged in');
+          } else {
+            console.log('Failed to log in');
+          }
+        })
+        .catch((error) => {
+          console.error('Failed to log in:', error);
+        });
+    }
+  }, [address]);
+
   return (
     <WagmiConfig client={wagmiClient}>
       <RainbowKitProvider chains={chains}>
