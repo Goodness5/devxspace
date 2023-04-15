e852fc780551b67c02dc8e1f043cc83806010540
vicsnet <adesanmiadebisi@gmail.com>
2023-04-15 11:56:38 +0100

    ongoing task

--- src/features/profile/component/OnGoingTask.jsx
+++ src/features/profile/component/OnGoingTask.jsx
@@ -47,10 +47,10 @@ const OnGoingTask = (props) => {
     <div className="mt-2 border-b-[1px] border-[#EFF2F9] mb-1 pb-2">
         <h5 className="text-[14px] pl-6  text-[#484679] font-semibold leading-4 ">{props.title}</h5>
 
-    <p className="text-[12px] pl-6 pr-2 text-[#484679] font-normalmt-2">{props.description}</p>
+    <p className="text-[14px] pl-6 pr-2 text-[#484679] font-normalmt-2">{props.description}</p>
     {
 !props.completed ?
-    <button onClick={Accept} className='text-[#FFFFFF] mt-4 ml-6 py-2 px-4 rounded-lg border-[1px] bg-[#052C5B] '>Submit Task</button> : <p>In Review</p>
+    <button onClick={Accept} className='text-[#FFFFFF] mt-4 ml-6 py-2 px-4 rounded-lg border-[1px] bg-[#052C5B] '>Submit Task</button> : <p className='text-[14px] pl-6'>In Review</p>
 }
 </div>
 
